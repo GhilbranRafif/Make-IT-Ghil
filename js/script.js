a901f4d2c20258492f6e708b0c25f58b74bcd32f
GhilbranMulya <220401023@student.umri.ac.id>
2024-05-05 12:27:25 +0700

    Second Commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Toogle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
