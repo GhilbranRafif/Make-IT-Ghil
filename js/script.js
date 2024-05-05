0c2b4952d66abdc0ab9805a6ef25dea33b07d750
GhilbranMulya <220401023@student.umri.ac.id>
2024-05-05 17:56:21 +0700

    Navbar Done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// tutup navbar diluar nav
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
